60bd754d2cbbca7a96b3d6b2111d71f3492bb4a0
Ayush <ayushcrudani@gmail.com>
2022-03-02 00:17:13 +0530

    Controller added

---
+++ src/main/java/def/Config.java
@@ -0,0 +1,10 @@
+package def;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan({"def"})
+public class Config {
+
+}
---
+++ src/main/java/def/WebInitializer.java
@@ -0,0 +1,27 @@
+package def;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class WebInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        // TODO Auto-generated method stub
+        return new Class[]{Config.class};
+//		return null;
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        // TODO Auto-generated method stub
+        return new String[]{"/"};
+//		return null;
+    }
+
+}
