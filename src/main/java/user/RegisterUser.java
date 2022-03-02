fcee11dc4018f7bf85d581e243a2cbffcba20779
Siddharth Vadgama <vadgamasiddharth9@gmail.com>
2022-03-02 15:48:08 +0530

    see this branch for clearing view resolver concept

--- src/main/java/user/RegisterUser.java
+++ src/main/java/user/RegisterUser.java
@@ -1,9 +1,9 @@
 package user;
 
 import def.FactoryProvider;
-import jakarta.servlet.*;
-import jakarta.servlet.http.*;
-import jakarta.servlet.annotation.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
