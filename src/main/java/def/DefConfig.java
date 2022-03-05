8bbe72a5cde8ff6c4861bafc331157ebe3242d57
Ayush <ayushcrudani@gmail.com>
2022-03-06 01:08:58 +0530

    update

--- src/main/java/def/DefConfig.java
+++ src/main/java/def/DefConfig.java
@@ -1,21 +1,21 @@
-package def;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-@Configuration
-@ComponentScan({"controllers"})
-public class DefConfig {
-    @Bean
-    public ViewResolver getViewResolver() {
-        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
-        resolver.setPrefix("/WEB-INF/view/");
-//        resolver.setPrefix("/");
-        resolver.setSuffix(".jsp");
-        return resolver;
-    }
-
-}
+//package def;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.ComponentScan;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.ViewResolver;
+//import org.springframework.web.servlet.view.InternalResourceViewResolver;
+//
+//@Configuration
+//@ComponentScan({"controllers"})
+//public class DefConfig {
+//    @Bean
+//    public ViewResolver getViewResolver() {
+//        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+//        resolver.setPrefix("/WEB-INF/view/");
+////        resolver.setPrefix("/");
+//        resolver.setSuffix(".jsp");
+//        return resolver;
+//    }
+//
+//}
